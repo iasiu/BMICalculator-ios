8864c22675f8dd84576a2edc605d36393778ea64
Jan Lewandowski <iasiu@icloud.com>
2020-04-12 15:21:01 +0200

    connected ui to view controller, display of values inputed with sliders, print bmi in console

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    var weight: Int = 100
+    var height: Float = 1.5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        heightLabel.text = "1.5m"
+        weightLabel.text = "100Kg"
     }
-
-
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        height = (Float(sender.value) * 100).rounded() / 100
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weight = Int(sender.value)
+        weightLabel.text = "\(weight)Kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let bmi = Float(weight) / (height * height)
+        print(bmi)
+    }
 }
 
